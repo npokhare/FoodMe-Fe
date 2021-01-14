d0769953ac798dd4a8aceb55bf8b432cc6222253
xeian_sangeet@hotmail.com <xeian_sangeet@hotmail.com>
2021-01-14 21:55:38 +0100

    add componetns and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SubmenuComponent } from './components/submenu/submenu.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { TileComponent } from './components/tile/tile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FooterComponent,
+    HeaderComponent,
+    SubmenuComponent,
+    TicketComponent,
+    TileComponent,
+    HomeComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule
